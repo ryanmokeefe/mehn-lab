a1d25f0dbe757194087db391a236256cbe11ed15
Ryan OKeefe <ryanokeefe208@gmail.com>
2017-12-21 11:22:17 -0500

    added views

---
+++ controllers/news.js
@@ -0,0 +1,8 @@
+const express       = require('express')
+// delete this line (replacing database with schema file): 
+// const db            = require('../db/connection')
+const Candidate     = require('../db/schema')
+const router        = express.Router()
+
+
+
