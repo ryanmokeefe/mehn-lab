b9b9eba63e2676598b728471063557e2646721d4
David Handel <dhandel1@hotmail.com>
2017-12-21 10:36:56 -0500

    updated index.js

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const news = require("./controllers/news")
 const app = express()
 const parser = require("body-parser")
 const methodOverride = require("method-override")
+const
 
 /// Port:
 app.set('port', process.env.PORT || 4001)
 app.set('view-engine', 'hbs')
+
+app.engine('.hbs', hbs({
+    extname: '.hbs',
+    partialsDir: 'views/',
+    layoutsDir: 'views/',
+    defaultLayout: 'layout-main'
+  }))
+  
