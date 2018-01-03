5532764da37f330acc145a8974369245720ff3c0
Kyric Livingston <ricingston@gmail.com>
2018-01-03 12:13:17 -0500

    savings

--- controllers/statics.js
+++ controllers/statics.js
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
-
-// GET /
-function home(req, res) {  
- res.render('index');
-}
-
-module.exports = {
- home: home,
-}
-=======
 // file for static pages
 
 // GET /
 function home(req, res) {  
     res.render('index');
   }
-  
   module.exports = {
     home: home,
   }
-  
->>>>>>> ca9c35dbf3c7bfb478ca9643afbd11257610e894
--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // { useMongoClient: true } no longer needed with mongoose v5
-mongoose.connect('mongodb://localhost/mehn-lab, { useMongoClient: true }')
+mongoose.connect('mongodb://localhost/mehn-lab',{ useMongoClient: true })
 
 mongoose.Promise = Promise
 
